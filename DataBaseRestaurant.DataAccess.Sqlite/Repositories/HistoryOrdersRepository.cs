eba9d9d2ff7b6641d607c93c2e94e566b4c9004b
sergxlove <sergxlove@mail.ru>
2024-11-02 20:19:12 +0600

    change file repositories

--- DataBaseRestaurant.DataAccess.Sqlite/Repositories/HistoryOrdersRepository.cs
+++ DataBaseRestaurant.DataAccess.Sqlite/Repositories/HistoryOrdersRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
+﻿namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
 {
-    internal class HistoryOrdersRepository
+    public class HistoryOrdersRepository
     {
     }
 }
--- DataBaseRestaurant.DataAccess.Sqlite/Repositories/WorkersRepository.cs
+++ DataBaseRestaurant.DataAccess.Sqlite/Repositories/WorkersRepository.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
+﻿namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
 {
-    internal class WorkersRepository
+    public class WorkersRepository
     {
     }
 }
