b0cb04696bd1b53c1e5e25084afcc805107f3920
sergxlove <sergxlove@mail.ru>
2024-11-07 01:50:53 +0600

    add all methods in repository

--- DataBaseRestaurant.DataAccess.Sqlite/Repositories/OrdersRepository.cs
+++ DataBaseRestaurant.DataAccess.Sqlite/Repositories/OrdersRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataBaseRestaurant.Core.Models;
+using DataBaseRestaurant.DataAccess.Sqlite.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
@@ -20,6 +21,55 @@ namespace DataBaseRestaurant.DataAccess.Sqlite.Repositories
 
             return ordersEntity.Select(a => Orders.Create(a.Id, a.TotalSum, a.ClientId, a.TableId).order)
                 .ToList()!;
+        }
+
+        public async Task<Orders?> GetById(int id)
+        {
+            var orderEntity = await _dbContext.Orders
+                .AsNoTracking()
+                .FirstOrDefaultAsync(a => a.Id == id);
+
+            if(orderEntity is not null)
+            {
+                return Orders.Create(orderEntity.Id, orderEntity.TotalSum, orderEntity.ClientId, orderEntity.TableId)
+                    .order;
+            }
+            return null;
+        }
+
+        public async Task<int> Add(Orders order)
+        {
+            OrdersEntity ordersEntity = new()
+            {
+                Id = order.Id,
+                TotalSum = order.TotalSum,
+                ClientId = order.ClientId,
+                TableId = order.TableId
+            };
+
+            await _dbContext.AddAsync(ordersEntity);
+            await _dbContext.SaveChangesAsync();
+            return ordersEntity.Id;
+        }
+
+        public async Task<int> Update(Orders order)
+        {
+            return await _dbContext.Orders
+                .AsNoTracking()
+                .Where(a => a.Id == order.Id)
+                .ExecuteUpdateAsync(s => 
+                s.SetProperty(s => s.Id, order.Id)
+                .SetProperty(s => s.TotalSum, order.TotalSum)
+                .SetProperty(s => s.ClientId, order.ClientId)
+                .SetProperty(s => s.TableId, order.TableId));
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await _dbContext.Orders
+                .AsNoTracking()
+                .Where(a => a.Id == id)
+                .ExecuteDeleteAsync();
         }
     }
 }
