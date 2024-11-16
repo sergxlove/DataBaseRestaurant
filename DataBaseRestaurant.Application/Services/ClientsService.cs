f9582cbb0888caff9c90ea15693c6d201410c862
sergxlove <sergxlove@mail.ru>
2024-11-17 00:03:48 +0600

    add new service

---
+++ DataBaseRestaurant.Application/Services/ClientsService.cs
@@ -0,0 +1,40 @@
+﻿using DataBaseRestaurant.Core.Abstraction.IRepository;
+using DataBaseRestaurant.Core.Models;
+
+namespace DataBaseRestaurant.Application.Services
+{
+    public class ClientsService
+    {
+        private readonly IClientsRepository _clientsRepository;
+
+        public ClientsService(IClientsRepository clientsRepository)
+        {
+            _clientsRepository = clientsRepository;
+        }
+
+        public async Task<List<Clients>> GetAllClients()
+        {
+            return await _clientsRepository.Get();
+        }
+
+        public async Task<Clients?> GetClientsById(int id)
+        {
+            return await _clientsRepository.GetById(id);
+        }
+
+        public async Task<int> AddNewClient(Clients clients)
+        {
+            return await _clientsRepository.Add(clients);
+        }
+
+        public async Task<int> UpdateClient(Clients clients)
+        {
+            return await _clientsRepository.Update(clients);
+        }
+
+        public async Task<int> DeleteClient(int id)
+        {
+            return await _clientsRepository.Delete(id);
+        }
+    }
+}
---
+++ DataBaseRestaurant.Application/Services/IngredientsService.cs
@@ -0,0 +1,40 @@
+﻿using DataBaseRestaurant.Core.Abstraction.IRepository;
+using DataBaseRestaurant.Core.Models;
+
+namespace DataBaseRestaurant.Application.Services
+{
+    public class IngredientsService
+    {
+        private readonly IIngredientsRepository _ingredientsRepository;
+
+        public IngredientsService(IIngredientsRepository ingredientsRepository)
+        {
+            _ingredientsRepository = ingredientsRepository;
+        }
+
+        public async Task<List<Ingredients>> GetAllIngredients()
+        {
+            return await _ingredientsRepository.Get();
+        }
+
+        public async Task<Ingredients?> GetIngredientsById(int id)
+        {
+            return await _ingredientsRepository.GetById(id);
+        }
+
+        public async Task<int> AddNewIngredients(Ingredients ingredients)
+        {
+            return await _ingredientsRepository.Add(ingredients);
+        }
+
+        public async Task<int> UpdateIngredients(Ingredients ingredients)
+        {
+            return await _ingredientsRepository.Update(ingredients);
+        }
+
+        public async Task<int> DeleteIngredients(int id)
+        {
+            return await _ingredientsRepository.Delete(id);
+        }
+    }
+}
