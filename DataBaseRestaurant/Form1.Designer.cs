ac15062efdd2e2dad99132655ee543c2c873d5ec
sergxlove <sergxlove@mail.ru>
2024-11-21 01:47:58 +0600

    add ui, create servicein form project

--- DataBaseRestaurant/Form1.Designer.cs
+++ DataBaseRestaurant/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            tabPage6 = new TabPage();
+            tabPage7 = new TabPage();
+            tabPage8 = new TabPage();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Controls.Add(tabPage5);
+            tabControl1.Controls.Add(tabPage6);
+            tabControl1.Controls.Add(tabPage7);
+            tabControl1.Controls.Add(tabPage8);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1195, 549);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1187, 516);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Clients";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1187, 516);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "HistoryOrders";
+            // 
+            // tabPage3
+            // 
+            tabPage3.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1187, 516);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Ingredients";
+            // 
+            // tabPage4
+            // 
+            tabPage4.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1187, 516);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Menu";
+            // 
+            // tabPage5
+            // 
+            tabPage5.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage5.Location = new Point(4, 29);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1187, 516);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "Orders";
+            // 
+            // tabPage6
+            // 
+            tabPage6.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage6.Location = new Point(4, 29);
+            tabPage6.Name = "tabPage6";
+            tabPage6.Size = new Size(1187, 516);
+            tabPage6.TabIndex = 5;
+            tabPage6.Text = "Suppliers";
+            // 
+            // tabPage7
+            // 
+            tabPage7.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage7.Location = new Point(4, 29);
+            tabPage7.Name = "tabPage7";
+            tabPage7.Size = new Size(1187, 516);
+            tabPage7.TabIndex = 6;
+            tabPage7.Text = "Tables";
+            // 
+            // tabPage8
+            // 
+            tabPage8.BackColor = Color.FromArgb(69, 69, 69);
+            tabPage8.Location = new Point(4, 29);
+            tabPage8.Name = "tabPage8";
+            tabPage8.Size = new Size(1187, 516);
+            tabPage8.TabIndex = 7;
+            tabPage8.Text = "Workers";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(69, 69, 69);
+            ClientSize = new Size(1219, 573);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private TabPage tabPage7;
+        private TabPage tabPage8;
     }
 }
