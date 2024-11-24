17e0ff0bb8b8ff0283f77a1957236d9c9e88f8a5
sergxlove <sergxlove@mail.ru>
2024-11-24 20:01:31 +0600

    add design form for workers and clients

--- DataBaseRestaurant/Form1.cs
+++ DataBaseRestaurant/Form1.cs
@@ -4,6 +4,7 @@ using DataBaseRestaurant.Core.Abstraction.IService;
 using DataBaseRestaurant.DataAccess.Sqlite;
 using DataBaseRestaurant.DataAccess.Sqlite.Repositories;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text;
 
 namespace DataBaseRestaurant
 {
@@ -12,27 +13,47 @@ namespace DataBaseRestaurant
         public Form1()
         {
             InitializeComponent();
-            serviceColection = new ServiceCollection();
-            serviceColection.AddDbContext<RestaurantDbContext>();
-            serviceColection.AddSingleton<IClientsRepository, ClientsRepository>();
-            serviceColection.AddSingleton<IHistoryOrdersRepository, HistoryOrdersRepository>();
-            serviceColection.AddSingleton<IIngredientsRepository, IngredientsRepository>();
-            serviceColection.AddSingleton<IMenuRepository, MenuRepository>();
-            serviceColection.AddSingleton<IOrdersRepository, OrdersRepository>();
-            serviceColection.AddSingleton<ISuppliersRepository, SuppliersRepository>();
-            serviceColection.AddSingleton<ITablesRepository, TablesRepository>();
-            serviceColection.AddSingleton<IWorkersRepository, WorkersRepository>();
-            serviceColection.AddSingleton<IClientsService, ClientsService>();
-            serviceColection.AddSingleton<IHistoryOrdersService, HistoryOrdersService>();
-            serviceColection.AddSingleton<IIngredientsService, IngredientsService>();
-            serviceColection.AddSingleton<IMenuService, MenuService>();
-            serviceColection.AddSingleton<IOrdersService, OrdersService>();
-            serviceColection.AddSingleton<ISuppliersService, SuppliersService>();
-            serviceColection.AddSingleton<ITablesService, TablesService>();
-            serviceColection.AddSingleton<IWorkersService, WorkersService>();
+            _serviceColection = new ServiceCollection();
+            _serviceColection.AddDbContext<RestaurantDbContext>();
+            _serviceColection.AddSingleton<IClientsRepository, ClientsRepository>();
+            _serviceColection.AddSingleton<IHistoryOrdersRepository, HistoryOrdersRepository>();
+            _serviceColection.AddSingleton<IIngredientsRepository, IngredientsRepository>();
+            _serviceColection.AddSingleton<IMenuRepository, MenuRepository>();
+            _serviceColection.AddSingleton<IOrdersRepository, OrdersRepository>();
+            _serviceColection.AddSingleton<ISuppliersRepository, SuppliersRepository>();
+            _serviceColection.AddSingleton<ITablesRepository, TablesRepository>();
+            _serviceColection.AddSingleton<IWorkersRepository, WorkersRepository>();
+            _serviceColection.AddSingleton<IClientsService, ClientsService>();
+            _serviceColection.AddSingleton<IHistoryOrdersService, HistoryOrdersService>();
+            _serviceColection.AddSingleton<IIngredientsService, IngredientsService>();
+            _serviceColection.AddSingleton<IMenuService, MenuService>();
+            _serviceColection.AddSingleton<IOrdersService, OrdersService>();
+            _serviceColection.AddSingleton<ISuppliersService, SuppliersService>();
+            _serviceColection.AddSingleton<ITablesService, TablesService>();
+            _serviceColection.AddSingleton<IWorkersService, WorkersService>();
+            _serviceProvider = _serviceColection.BuildServiceProvider();
         }
 
-        private ServiceCollection serviceColection;
+        private ServiceCollection _serviceColection;
+        private ServiceProvider _serviceProvider;
 
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var clientService = _serviceProvider.GetService<IClientsService>();
+            var clients = await clientService!.GetAllClientsAsync();
+            StringBuilder clientString = new StringBuilder();
+            foreach (var client in clients)
+            {
+                clientString.Append($"ID: {client.Id} \r\nName: {client.Name} \r\nEmail: {client.Email}\r\n" +
+                    $"Preferences: {client.Preferences} \r\nOrderID: {client.OrderId} \r\n");
+                clientString.Append("\r\n");
+            }
+            textBox1.Text = clientString.ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var workersService = _serviceProvider.GetService<IWorkersService>();
+        }
     }
 }
